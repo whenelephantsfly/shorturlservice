6609d3f2e896f80772f980c94820fd9b63eb2df4
manthana <manthana@vmware.com>
2021-07-22 16:55:42 +0530

    Added docker file to contanerize the App

--- projectFiles/shortUrlService/hashing/urls.py
+++ projectFiles/shortUrlService/hashing/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'generateShortUrl', views.generate_short_url, name='generateShortUrl'),
     url(r'deleteUrlData', views.delete_url_data, name='deleteUrlData'),
-    url(r'getShortUrl', views.get_short_url, name='getShortUrl'),
+    url(r'getOriginalUrl', views.get_original_url, name='getOriginalUrl'),
     # Remaining regular expression for url matching
     url(r'regularExpression to redirect remaining', views.redirect, name='redirect'),
 ]
--- projectFiles/shortUrlService/hashing/views.py
+++ projectFiles/shortUrlService/hashing/views.py
@@ -1,39 +1,62 @@
+import django.core.validators
 from django.shortcuts import render
 from django.http import HttpResponse
+from pymongo import MongoClient
 import hashlib
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+import socket
 
 
-def generate_short_url(request):
-    # Remaining -- Check if tinyurl is given
-    url = "https://www.python.org/dev/peps/pep-0008/#function-and-variable-names"
-    key32 = hashlib.md5(url.encode()).hexdigest()
-    tinyUrl = key32[:8]
-
-    while(True):
-        # Check database if key is present
-        flag = get_short_url()
-
-        if not flag:
-            # Database call to
+connect_string = 'mongodb+srv://mukul:samplepassword@cluster0.2ergm.mongodb.net/test'
+db_client = MongoClient(connect_string)
+dbname = db_client['shortURLDB']
+url_collection = dbname['URL']
 
 
-
-
-
-    return True
-
-
-def delete_url_data(request):
-
-
+def get_short_url(tiny_url):
     pass
 
 
-def get_short_url(request):
+def generate_short_url(request):
+
+    url = "https://www.python.org/dev/peps/pep-0008/#function-and-variable-names"
+    # Check if Given Url is valid
+    try:
+        URLValidator()(url)
+    except ValidationError  as  exception:
+        return HttpResponse("Given url is not valid")
+
+    # Remaining -- Check if tinyurl is given
+
+
+    # Converting the URL to 32 bit MD5 hash value.
+    key32 = hashlib.md5(url.encode()).hexdigest()
+    tiny_url = key32[:8]
+
+
+    for i in range(4):
+        # Check database if key is present
+        flag = get_short_url(tiny_url)
+        if not flag:
+
+            return HttpResponse(socket.gethostname()+tiny_url)
+
+
+
+
+def get_original_url(request):
+    pass
+
+
+def delete_url_data(request):
+    docs = url_collection.find({})
+    print(docs)
+    for r in docs:
+        print(r)
     pass
 
 
 def redirect(request):
     # Check if tiny url is given
-
     pass

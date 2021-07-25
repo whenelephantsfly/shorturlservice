5599ea34a990ffe27e1e643d66bf5fa18a8e8e90
Mukul Lalwani <mlalwani@vmware.com>
2021-07-25 18:38:12 +0530

    Integrated UI with backend

--- projectFiles/short-url-ui/src/app/convert-url/convert-url.component.ts
+++ projectFiles/short-url-ui/src/app/convert-url/convert-url.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ConvertUrlComponent implements OnInit {
 
   longUrl: string = "";
+  showInput: boolean = true;
   responseData: any = {};
 
   constructor() { }
@@ -16,19 +17,48 @@ export class ConvertUrlComponent implements OnInit {
   }
 
   onSubmit() {
-    let formData = new FormData();
-    formData.append('url', this.longUrl);
+    let postData = {
+      "url": this.longUrl
+    }
+
+    // this.http.post('/api/generateShortUrl', {url: this.longUrl}).subscribe(response => {
+    //   this.responseData = response;
+    //   console.log(response);
+    // });
 
     fetch("/api/generateShortUrl", {
       method: "POST",
-      body: JSON.stringify(formData)
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({"url": this.longUrl})
     }).then(response => response.json())
     .then(data => {
       this.responseData = data;
-      this.longUrl = '';
+      this.longUrl = this.responseData.shortURL;
+      this.showInput = false;
       console.log(this.responseData);
     })
     .catch(e => console.error(e));
+  }
+
+  changed() {
+    this.showInput = true;
+    console.log("Changed");
+  }
+
+  copyLink() {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.longUrl;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
   }
 
 }

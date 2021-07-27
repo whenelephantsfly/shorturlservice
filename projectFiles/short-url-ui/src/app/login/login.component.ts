6294281f7d0ad3cb62a588a319b5ce4cdc9cab81
Megha <mmoon@vmware.com>
2021-07-27 15:11:52 +0530

    UI updates

--- projectFiles/short-url-ui/src/app/login/login.component.ts
+++ projectFiles/short-url-ui/src/app/login/login.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: string = "";
+  password: string = "";
+  inputValid:boolean=true;
+  rememberMe:  boolean=false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  validate()
+  {
+    
+    if(this.username=="" || this.password==""){
+      
+      this.inputValid=false;
+    }
+    else{
+      this.inputValid=true;
+      location.href = "/convert-url";
+    }
+    
+  }
+
 
 }
--- projectFiles/short-url-ui/src/app/signup/signup.component.ts
+++ projectFiles/short-url-ui/src/app/signup/signup.component.ts
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  username: string = "";
+  password: string = "";
+  confirmpassword: string="";
+  emailid: string="";
+  inputs: boolean=true;
+  pass_copass:boolean=true;
+  user_already_exist:boolean=true;
   constructor() { }
 
   ngOnInit(): void {
   }
+  signupvalidate()
+  {
 
+    if((this.username=="Megha" || this.emailid=="meghamoon02@gmail.com") &&
+    this.username!="" && this.emailid!=""){
+      
+      this.user_already_exist=false;
+    }
+    else{
+      this.user_already_exist=true;
+    }  
+
+    
+    if(this.username=="" || this.emailid=="" || this.password=="" || this.confirmpassword==""){
+      
+      this.inputs=false;
+    }
+    else{
+      this.inputs=true;
+    }
+
+    
+
+    if(this.password!=this.confirmpassword  &&
+    this.username!="" && this.emailid!="" && this.password!="" && this.confirmpassword!="")
+    {
+      this.pass_copass=false;
+    }
+    else{
+      this.pass_copass=true;
+    }
+
+
+    
+  }
 }
